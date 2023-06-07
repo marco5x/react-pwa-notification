77d47e8504ac974de335f68fdf1d596ce9b9bbde
MarcosX <79601596+Marco5X@users.noreply.github.com>
2023-06-07 17:22:49 -0300

    Finish version

--- src/service-worker.js
+++ src/service-worker.js
@@ -81,37 +81,3 @@ self.addEventListener("push", (event) => {
   //console.log(event.data.json());
   self.registration.showNotification(title, { body: message });
 });
-
-//********************/// ADD SW////************** *//
-const CACHE_NAME = "cool-cache";
-
-// Add whichever assets you want to precache here:
-const PRECACHE_ASSETS = ["/public/", "/src/"];
-
-// Listener for the install event - precaches our assets list on service worker install.
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    (async () => {
-      const cache = await caches.open(CACHE_NAME);
-      cache.addAll(PRECACHE_ASSETS);
-    })()
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(async () => {
-    const cache = await caches.open(CACHE_NAME);
-
-    // match the request to our cache
-    const cachedResponse = await cache.match(event.request);
-
-    // check if we got a valid response
-    if (cachedResponse !== undefined) {
-      // Cache hit, return the resource
-      return cachedResponse;
-    } else {
-      // Otherwise, go to the network
-      return fetch(event.request);
-    }
-  });
-});
